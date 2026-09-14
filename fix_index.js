f26bb07daad1fb8e6f9bf6da6e69100513aa50f2
DimaKonge <pawlukdima@gmail.com>
2026-09-14 14:28:48 +0000

    chore: clean up temporary refactoring scripts

--- fix_index.js
+++
@@ -1,4 +0,0 @@
-import fs from 'fs';
-let content = fs.readFileSync('client/index.html', 'utf8');
-content = content.replace(/<script[^>]*src="%VITE_ANALYTICS_ENDPOINT%\/umami"[^>]*><\/script>/gi, '');
-fs.writeFileSync('client/index.html', content);
